9fb84577f42d05bdd905d77ce662d0a76ae79a25
DanielOpic <danielopic@gmail.com>
2025-02-22 18:15:48 +0100

    Add Angular Material and some styles to sitebar

--- frontend/src/app/pages/website/layout/elements/sitebar/sitebar.module.ts
+++ frontend/src/app/pages/website/layout/elements/sitebar/sitebar.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
 import { SitebarComponent } from './sitebar.component';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SitebarComponent],
-  imports: [CommonModule, RouterOutlet, RouterModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+  ],
   exports: [SitebarComponent],
 })
 export class SitebarModule {}
