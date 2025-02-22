5fa4237864804c4e37a0ae085ed285624164ad73
DanielOpic <danielopic@gmail.com>
2025-02-22 16:12:14 +0100

    home and about added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // Komponent standalone
+  imports: [RouterOutlet, RouterModule], // Importuje inne zależności, np. RouterOutlet
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'frontend';
